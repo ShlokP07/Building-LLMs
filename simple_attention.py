6ed22a08a8c5db396be22621f93f5f559d905a47
ShlokP07 <shlok.pik@gmail.com>
2025-01-30 21:38:08 -0500

    Completed logic of simple attention

--- simple_attention.py
+++ simple_attention.py
@@ -1,21 +1,30 @@
 import torch
 
-def calculate_attention_scores(query, inputs):
+def get_attention_scores(inputs):
     '''
     init an attention tensor
     calculate the dot product between query and each word
     '''
-    attention_scores = torch.empty(inputs.shape[0])
-    for idx, x in enumerate(inputs):
-        attention_scores[idx] = torch.dot(x, query)
+    attention_scores = torch.empty(inputs.shape[0], inputs.shape[0])
+    attention_scores = torch.matmul(inputs, inputs.T)
+    
+    # for i, x_i in enumerate(inputs):
+    #     for j, x_j in enumerate(inputs):
+    #         attention_scores[i, j] = torch.dot(x_i, x_j)
     
     return attention_scores
 
-def calculate_attention_weights(attention_scores):
+def get_attention_weights(attention_scores):
     '''
     convert raw scores that sum up to 1
     '''
-    return torch.softmax(attention_scores, dim=0) # remember to calculate softmax along each row
+    return torch.softmax(attention_scores, dim=-1) # we tell pytorch to apply softmax on the last dim, robust, works well at higher dim
+
+def get_context_vector(inputs, attention_weights):
+    '''
+    multiply each index of each imput word with its attention weight and then add
+    '''
+    return torch.matmul(attention_weights, inputs) # remember when matmul you have to match inner dimensions thats y this order
 
 def main():
     inputs = torch.tensor(
@@ -27,12 +36,13 @@ def main():
     [0.05, 0.80, 0.55]] # step    
     )
 
-    query = inputs[1] 
     
-    attention_scores = calculate_attention_scores(query, inputs)
-    attention_weights = calculate_attention_weights(attention_scores)
+    attention_scores = get_attention_scores(inputs)
+    attention_weights = get_attention_weights(attention_scores)
+    convext_vector = get_context_vector(attention_weights)
     print(f'attention scores: {attention_scores}')
     print(f'attention weights: {attention_weights}')      
+    print(f'context vector: {convext_vector}')     
 
     
 if __name__ == '__main__':
