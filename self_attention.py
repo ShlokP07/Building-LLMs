77f362afe28e7f82054f3922dfd365846282b8ff
ShlokP07 <shlok.pik@gmail.com>
2025-02-01 21:22:37 -0500

    added a simple attention mechanism

--- self_attention.py
+++ self_attention.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
+import torch 
 
-class selfattention(nn.Moudule):
+class selfattention(nn.Module):
     def __init__(self, dim_in, dim_out, qkv_bias = False):
         super().__init__()
         
@@ -8,11 +9,21 @@ class selfattention(nn.Moudule):
         WE implement Q, V, K as a linear layer instaead of matrix as they are
         easier and better to train
         '''
-        q_W = nn.Linear(dim_in, dim_out, bias= qkv_bias)
-        V_W = nn.Linear(dim_in, dim_out, bias= qkv_bias)
-        K_W = nn.Linear(dim_in, dim_out, bias= qkv_bias)
+        self.Q_W = nn.Linear(dim_in, dim_out, bias= qkv_bias)
+        self.V_W = nn.Linear(dim_in, dim_out, bias= qkv_bias)
+        self.K_W = nn.Linear(dim_in, dim_out, bias= qkv_bias)
         
     def forward(self, x):
-        pass
+        queries = self.Q_w(x)
+        keys = self.K_W(x)
+        values = self.V_W(x)
+        
+        attention_scores = torch.matmul(queries, keys.T)
+        attention_weights  = torch.softmax(attention_scores/ keys[-1]**0.5, dim = -1)
+        context_vector = torch.matmul(attention_weights, values)
+        
+        return context_vector
+        
+        
     
     
